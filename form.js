fa2008e4e5e6eb0a9d8c44d31d5e86218fa48138
ANON4620 <swarnadeep.click@gmail.com>
2021-04-15 23:12:48 +0530

    Documentation, Added Form Validation

--- form.js
+++ form.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   
-  const note = document.querySelector('#note');
   const title = document.querySelector('#title');
-  const button = document.querySelector('#btn');
+  const note = document.querySelector('#note');
+  const submit = document.querySelector('#btn');
   
-  const height = window.innerHeight - 400;
+  const height = window.innerHeight - 400; // sets the hieght of the element #note
   
   
-  note.style.height = `${height}px`; // sets the hieght of the element #note
+  note.style.height = `${height}px`;
   
   
   let notes = JSON.parse(localStorage.getItem('notes'));
@@ -15,13 +15,31 @@ document.addEventListener('DOMContentLoaded', () => {
   if(notes === null)
     notes = [];
   
-  button.onclick = () => {
+  
+  title.onclick = () => {
+    title.style.borderBottom = '2px solid black';
+  }
+  
+  note.onclick = () => {
+    note.style.border = '2px solid black';
+  }
+  
+  submit.onclick = () => {
     
     const obj = {};
-    obj.title = title.value;
-    obj.text = note.value.trim();
-    if(obj.text !== '') {
-      obj.text = `<pre>${obj.text}</pre>`;
+    obj.title = title.value.trim();
+    obj.note = note.value.trim();
+    
+    // check if the form is empty
+    if(obj.title === '' || obj.note === '') {
+      navigator.vibrate(100);
+      if(obj.title === '')
+        title.style.borderBottom = '2px solid red';
+      if(obj.note === '')
+        note.style.border = '2px solid red';
+    }
+    else {
+      obj.note = `<pre>${obj.note}</pre>`;
       notes.push(obj);
       localStorage.setItem('notes', JSON.stringify(notes));
       
@@ -30,6 +48,7 @@ document.addEventListener('DOMContentLoaded', () => {
       
       window.location.href = 'index.html';
     }
-    
   }
 });
+
+
--- home.js
+++ home.js
@@ -1,75 +1,28 @@
 const note = JSON.parse(localStorage.getItem('notes'));
 
-function readMore(id) {
-  const id_number = Number(id.slice(9)); // getting id number
-  document.getElementById(`text${id_number}`).innerHTML = note[id_number].text; // expanding the note
-}
-
-function deleteNote(id) {
-  document.getElementById(id).parentElement.remove(); // remove element from DOM
-  
-  const notes_len = note.length; // array length
-  const mid_index_notes = notes_len-1 / 2; // middle index of the array
-  
-  const temp = note[id]; // hold the element in array note with index [ id] in a temporary variable
-  // if id > middle index of notes array
-  if(id > mid_index_notes) {
-    // move the element in array with index [id] to the last position of the array
-    for(let i = id; i < notes_len-1; i++)
-      note[i] = note[i + 1]; // move each element of the second half of the array to its previous position
-    note[notes_len-1] = temp; // push the element to the last position of the array
-    note.pop(); // throw the last element out of the array
-  }
-  else {
-    // move the element in array with index [id] to the last position of the array
-    for(let i = id; i > 0; i--)
-      note[i] = note[i - 1]; // move each element of the first half of the array to its next position
-    note[0] = temp; // push the element to the first position of the array
-    note.shift(); // throw the first element out of the array
-  }
-  
-  // if note is a blank array
-  if(note.length === 0) {
-    localStorage.removeItem('notes'); // remove notes from localStorage
-    
-    // display background image and notes will be appearing here
-    document.querySelector('.container').innerHTML = 
-    `<img src="bg.svg" class="background" alt="background image" preload="auto">
-    <div class="header">Notes will be appearing here</div>`;
-  }
-  else
-    localStorage.setItem('notes', JSON.stringify(note)); // push the new note array to localStorage
-}
-
-
 document.addEventListener('DOMContentLoaded', () => {
   
-  
-  document.querySelector('button').onclick = () => {window.location.href = 'form.html';}
+  document.querySelector('#add-items').onclick = () => {window.location.href = 'form.html';}
 
   if(note !== null) {
-
     const container = document.querySelector('.container');
-    const notes_len = note.length;
+    const len = note.length;
     
     container.innerHTML = '';
     
-    
-    for(let i = 0; i < notes_len; i++) {
+    for(let i = 0; i < len; i++) {
       
-      let text_words = note[i].text.split(" "); // Get an array of the string
-      let text = '';
-      
+      const words = note[i].note.split(" "); // Get an array of the string
+      let text; 
       // if no. of words in the array <= 50
-      if(text_words.length <= 50) {
-        text = note[i].text; // the full note
-      }
+      if(words.length <= 50)
+        text = note[i].note; // the full note
       // if no. of words > 50
       else {
-        text = text_words.slice(0, 50).join(' '); // Get the first 50 words
-        text += `...<button id="read-more${i}" class="read-more" onclick="readMore(this.id)">Read more</button>`; // Adding read more button
+        text = words.slice(0, 50).join(' '); // Get the first 50 words
+        text += `...<button id="read-more${i}" class="read-more" onclick="readMore(this.id)">Read more</button>`; // Add read more button
       }
-
+      
       container.innerHTML += 
       `<div class="note">
       
@@ -81,13 +34,58 @@ document.addEventListener('DOMContentLoaded', () => {
           ${note[i].title}
         </div>
         
-        <div id="text${i}"" class="text">
+        <div id="text${i}" class="text">
           ${text}
         </div>
         
       </div>`;
-
+      
       // .delBtnImg src --> https://icons8.com/icons/set/delete-button
     }
   }
 });
+
+
+
+
+
+function readMore(id) {
+  const id_number = Number(id.slice(9)); // getting id number
+  document.getElementById(`text${id_number}`).innerHTML = note[id_number].note; // expanding the note
+}
+
+function deleteNote(id) {
+  document.getElementById(id).parentElement.remove(); // remove element from DOM
+
+  const len = note.length; // array length
+  const mid_index_notes = len - 1 / 2; // middle index of the array
+
+  const temp = note[id]; // hold the element in array note with index [ id] in a temporary variable
+  // if id > middle index of notes array
+  if(id > mid_index_notes) {
+    // move the element in array with index [id] to the last position of the array
+    for(let i = id; i < len - 1; i++)
+      note[i] = note[i + 1]; // move each element of the second half of the array to its previous position
+    note[len - 1] = temp; // push the element to the last position of the array
+    note.pop(); // throw the last element out of the array
+  }
+  else {
+    // move the element in array with index [id] to the last position of the array
+    for(let i = id; i > 0; i--)
+      note[i] = note[i - 1]; // move each element of the first half of the array to its next position
+    note[0] = temp; // push the element to the first position of the array
+    note.shift(); // throw the first element out of the array
+  }
+
+  // if note is a blank array
+  if(note.length === 0) {
+    localStorage.removeItem('notes'); // remove notes from localStorage
+
+    // display background image and notes will be appearing here
+    document.querySelector('.container').innerHTML =
+      `<img src="bg.svg" class="background" alt="background image" preload="auto">
+    <div class="header">Notes will be appearing here</div>`;
+  }
+  else
+    localStorage.setItem('notes', JSON.stringify(note)); // push the new note array to localStorage
+}
